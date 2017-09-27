f5b4e4c69e118d1fab74891b6096a316f1fd0fa5
mr.dd33 <dimantrigun@mail.ru>
2017-09-27 17:00:00 +0300

    Minimum code duplication

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,22 +1,42 @@
 package com.acme.edu;
 
 public class Logger {
-    static String textBeforeLog = "primitive ";
+
+    public static String getTextBeforeLog(Object message) {
+        String textBeforeLog = "";
+
+        if(message != null) {
+            if(String.class.equals(message.getClass())) {
+                textBeforeLog = "string: ";
+            }
+            else if(Character.class.equals(message.getClass())) {
+                textBeforeLog = "char: ";
+            }
+            else if(Integer.class.equals(message.getClass()) ||
+                    Byte.class.equals(message.getClass())) {
+                textBeforeLog = "primitive: ";
+            }
+        }
+
+
+        return textBeforeLog;
+    }
+
 
     public static void log(String message) {
-        System.out.println("string: " + message);
+        System.out.println(getTextBeforeLog(message) + message);
     }
 
     public static void log(char message) {
-        System.out.println("char: " + message);
+        System.out.println(getTextBeforeLog(message) + message);
     }
 
     public static void log(int message) {
-        System.out.println(textBeforeLog + message);
+        System.out.println(getTextBeforeLog(message) + message);
     }
 
     public static void log(byte message) {
-        System.out.println(textBeforeLog + message);
+        System.out.println(getTextBeforeLog(message) + message);
     }
 
     public static void main(String[] args) {
